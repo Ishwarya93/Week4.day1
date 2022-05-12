0db4d2ed6ac46cfb6726920a6438c266a965c981
Ishwarya93 <103831149+Ishwarya93@users.noreply.github.com>
2022-05-12 15:04:55 +0530

    Add files via upload

---
+++ TableCount.java
@@ -0,0 +1,28 @@
+package week4.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TableCount {
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://html.com/tags/table/");
+		int tablecount = driver.findElements(By.xpath("//table)")).size();
+		System.out.println("Total Number of tables is " + tablecount);
+		System.out.println("------------------------------------");
+		for (int i = 1; i <= tablecount; i++) {
+			int table1rowcount = driver.findElements(By.xpath("//table)[" + i + "]//tr")).size();
+			System.out.println("The Total rows in the table is" + i + "is " + table1rowcount);
+			int table1colcount = driver.findElements(By.xpath("//table)[" + i + "]//th")).size();
+			System.out.println("The Total column in the table is" + i + "is " + table1colcount);
+		}
+		driver.close();
+
+}
+}
